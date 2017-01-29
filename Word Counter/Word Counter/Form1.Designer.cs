5709a2ea9545244dfc2b2d3923ee8a7e0a662021
skillfulshoe <pschuw@gmail.com>
2017-01-29 15:26:12 -0600

    Add files via upload

---
+++ Word Counter/Word Counter/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Word_Counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.lblWordCount = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.AccessibleName = "btnUpload";
+            this.btnUpload.Location = new System.Drawing.Point(13, 13);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Enabled = false;
+            this.lblFileName.Location = new System.Drawing.Point(94, 18);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(54, 13);
+            this.lblFileName.TabIndex = 1;
+            this.lblFileName.Text = "File Name";
+            // 
+            // lblWordCount
+            // 
+            this.lblWordCount.AutoSize = true;
+            this.lblWordCount.Location = new System.Drawing.Point(13, 43);
+            this.lblWordCount.Name = "lblWordCount";
+            this.lblWordCount.Size = new System.Drawing.Size(67, 13);
+            this.lblWordCount.TabIndex = 2;
+            this.lblWordCount.Text = "Word Count:";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(97, 42);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 13);
+            this.lblCount.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(260, 87);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblWordCount);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "Form1";
+            this.Text = "Word Count";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label lblWordCount;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ Word Counter/Word Counter/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Word_Counter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            int intCount = 0;
+            string strPath;
+            //Open the file
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get path of the file and counts the words per line
+                strPath = theDialog.FileName;
+                string[] filelines = File.ReadAllLines(strPath);
+
+                foreach (string line in filelines)
+                {
+                    intCount += line.Split(' ').Length;
+                }
+                string strFileName = Path.GetFileName(strPath);
+                lblFileName.Text = strFileName;
+                lblCount.Text = intCount.ToString();
+            }
+            else
+            {
+                lblFileName.Text = "Error";
+            }
+
+        }
+    }
+}
